df1624fb7846e8bd440dbcf2bd3202b8a848ba8a
Jan Francksen <janfrancksen@MacBook-Pro-von-Jan.fritz.box>
2021-08-01 16:54:16 +0200

    v. 0.3.4

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         typekit: {
-          id: process.env.gps2ond,
+          id: process.env.TYPEKIT_ID,
         },
       },
     }
